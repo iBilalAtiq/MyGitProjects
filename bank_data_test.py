d3507eb29b5e84165740494502a0546e49d022b4
Your Name <bilalatiqsd@gmail.com>
2025-01-29 23:56:34 +0100

    avg_Transaction_Amount

--- bank_data_test.py
+++ bank_data_test.py
@@ -14,13 +14,15 @@ df = spark.read.option("header", "true").csv(file_path)
 
 # Data Preprocessing: Convert columns to appropriate data types
 df = df.withColumn("Account_Balance", df["Account_Balance"].cast("float"))
+df = df.withColumn("Transaction_Amount", df["Transaction_Amount"].cast("float"))
 
 # 1. Grouping by Country and calculating the average salary
 avg_account_balance = df.agg(F.avg("Account_Balance").alias("Average_Account_Balance"))
+avg_Transaction_Amount = df.agg(F.avg("Transaction_Amount").alias("Average_Transaction_Amount"))
 
 # Write the result to a single CSV file in GCS
 avg_account_balance.coalesce(1).write.mode("overwrite").option("header", "true").csv("gs://siddiq_buc/output_new")
-
+avg_Transaction_Amount.coalesce(1).write.mode("overwrite").option("header", "true").csv("gs://siddiq_buc/output_new")
 
 # Stop the Spark session
 spark.stop()
